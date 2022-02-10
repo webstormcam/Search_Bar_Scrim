726ef61c447cdca31284c01df9cf235dac2d6c2c
Cameron Chandler <webstormcam@gmail.com>
2022-02-10 08:16:23 -0500

    Updated with the final version of the search bar

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let input = document.getElementById('usernameInput')
-
-input.addEventListener('keyup',function(event){
-let username = event.target.value
-console.log(username.toLowerCase())
+document.getElementById("searchInput").addEventListener("keyup", function(event) {
+    let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+    
+    for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+        const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
+       if(currentName.includes(searchQuery)){
+           allNamesDOMCollection[counter].style.display='block'
+       } else{
+           allNamesDOMCollection[counter].style.display='none'
+       }
+    }
 });
